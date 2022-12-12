d99817dc01e5976e5b8ac9e714f7040423c367e2
arjunkalidoss <arjunkalidoss@gmail.com>
2022-12-12 13:42:18 -0500

    iniital commit

--- Jenkins/configure.groovy
+++ Jenkins/configure.groovy
@@ -1,6 +1,25 @@
 pipeline {
-  project: 'arjunkalidoss',
-  repo: 'learn',
-  file: ['Jenkins/vars','Jenkins/configure']
-  branch: 'main'
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Hello World'
+            }
+        }
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git branch: 'main',
+                    url: 'https://github.com/arjunkalidoss/learn.git'
+            }
+        }
+        stage('Execute Ansible') {
+            steps {
+                // Execute Ansible playbook
+                ansible-playbook '/var/lib/jenkins/workspace/pipe/Ansible/playbook/verify-connection.yml' -vv
+            }
+        }
+    }
+
 }
